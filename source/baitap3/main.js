a2d5a63a223113ae92fa5de70aa8fdf6e0bdd2bd
Mark <81944563+TungNguyen304@users.noreply.github.com>
2023-02-01 15:55:22 +0700

    Fix code(change doubleqoute to singleqoute, add space after if keyword)

--- source/baitap3/main.js
+++ source/baitap3/main.js
@@ -1,25 +1,25 @@
 const $ = document.querySelector.bind(document);
-const screen = $(".screen img");
-const start = $(".action .start");
-const end = $(".action .stop");
+const screen = $('.screen img');
+const start = $('.action .start');
+const end = $('.action .stop');
 
 const imgList = ['img1.jfif', 'img2.jfif', 'img3.jfif', 'img4.jpg', 'img5.jpg', 'img6.jfif', 'img7.jpg', 'img8.jfif', 'img9.jfif', 'img10.jpg'];
 let slide = undefined;
 let index = undefined;
 
 start.onclick = () => {
-    if(!slide) {
+    if (!slide) {
         slide = setInterval(() => {
         let randomNumber = randomMethod();
         do {
-            if(randomNumber === index) {
+            if (randomNumber === index) {
                 randomNumber = randomMethod();
             } else {
                 break;
             }
         } while(true)
         index = randomNumber;
-        screen.src = "./images/" + imgList[randomNumber];
+        screen.src = './images/' + imgList[randomNumber];
         }, 1000);
     }
 };
