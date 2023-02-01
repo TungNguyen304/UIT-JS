fc107a6683eb37a51c80fb803cd546451fb44878
Mark <81944563+TungNguyen304@users.noreply.github.com>
2023-02-01 14:50:58 +0700

    Fix code(change doubleqoute to singleqoute)

--- source/baitap2/main.js
+++ source/baitap2/main.js
@@ -1,11 +1,11 @@
 const $$ = document.querySelectorAll.bind(document);
 const $ = document.querySelector.bind(document);
 
-const content = $(".form__content textarea");
-const screen = $(".screen");
-const color = document.getElementById("text");
-const background = document.getElementById("background");
-const btnList = $$(".form_item .style input");
+const content = $('.form__content textarea');
+const screen = $('.screen');
+const color = document.getElementById('text');
+const background = document.getElementById('background');
+const btnList = $$('.form_item .style input');
 
 content.onkeyup = (e) => {
   screen.textContent = e.target.value;
@@ -21,25 +21,25 @@ background.oninput = (e) => {
 
 btnList.forEach((item) => {
   item.onclick = (e) => {
-    if (e.target.value === "Italic") {
-      if (screen.style.fontStyle === "italic") {
-        screen.style.fontStyle = "normal";
+    if (e.target.value === 'Italic') {
+      if (screen.style.fontStyle === 'italic') {
+        screen.style.fontStyle = 'normal';
       } else {
-        screen.style.fontStyle = "italic";
+        screen.style.fontStyle = 'italic';
       }
     }
-    if (e.target.value === "Bold") {
-      if (screen.style.fontWeight === "bold") {
-        screen.style.fontWeight = "normal";
+    if (e.target.value === 'Bold') {
+      if (screen.style.fontWeight === 'bold') {
+        screen.style.fontWeight = 'normal';
       } else {
-        screen.style.fontWeight = "bold";
+        screen.style.fontWeight = 'bold';
       }
     }
-    if (e.target.value === "Underline") {
-      if (screen.style.textDecoration === "underline") {
-        screen.style.textDecoration = "none";
+    if (e.target.value === 'Underline') {
+      if (screen.style.textDecoration === 'underline') {
+        screen.style.textDecoration = 'none';
       } else {
-        screen.style.textDecoration = "underline";
+        screen.style.textDecoration = 'underline';
       }
     }
   };
