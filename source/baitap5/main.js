13e1441a5f9587180724ad1b09b965b8dd4e52b4
Mark <81944563+TungNguyen304@users.noreply.github.com>
2023-02-02 19:56:30 +0700

    Fix code(change doubleqoute to singleqoute)

--- source/baitap5/main.js
+++ source/baitap5/main.js
@@ -14,79 +14,103 @@ let msgStoreLeft = [];
 let msgStoreRight = [];
 
 actionLeft.forEach((item) => {
-    handleReStyleContent(item, inputLeft, 'left');
-})
+  handleReStyleContent(item, inputLeft, 'left');
+});
 
 actionRight.forEach((item) => {
-    handleReStyleContent(item, inputRight, 'right');
-})
+  handleReStyleContent(item, inputRight, 'right');
+});
 
 submitLeft.onclick = sendMsgForLeft;
 inputLeft.onkeydown = function (e) {
-    if (e.key === 'Enter' && e.keyCode === 13) {
-        e.preventDefault();
-        if (this.value) {
-            sendMsgForLeft();
-        }
+  if (e.key === 'Enter' && e.keyCode === 13) {
+    e.preventDefault();
+    if (this.value) {
+      sendMsgForLeft();
     }
-}
+  }
+};
 
 submitRight.onclick = sendMsgForRight;
 inputRight.onkeydown = function (e) {
-    if (e.key === 'Enter' && e.keyCode === 13) {
-        e.preventDefault();
-        if (this.value) {
-            sendMsgForRight();
-        }
+  if (e.key === 'Enter' && e.keyCode === 13) {
+    e.preventDefault();
+    if (this.value) {
+      sendMsgForRight();
     }
-}
+  }
+};
 
 function handleReStyleContent(item, actor, side) {
-    item.onclick = () => {
-        if (item.textContent === 'Đậm') {
-            if (actor.style.fontWeight === 'bold') {
-                actor.style.fontWeight = 'unset';
-            } else {
-                actor.style.fontWeight = 'bold';
-            }
+  item.onclick = () => {
+    if (item.textContent === 'Đậm') {
+      if (actor.style.fontWeight === 'bold') {
+        actor.style.fontWeight = 'unset';
+      } else {
+        actor.style.fontWeight = 'bold';
+      }
+    } else {
+      if (item.textContent === 'Nghiêng') {
+        if (actor.style.fontStyle === 'italic') {
+          actor.style.fontStyle = 'unset';
         } else {
-            if (item.textContent === 'Nghiêng') {
-                if (actor.style.fontStyle === 'italic') {
-                    actor.style.fontStyle = 'unset';
-                } else {
-                    actor.style.fontStyle = 'italic';
-                }
-            } else {
-                if (side === 'left') {
-                    msgStoreLeft = [];
-                    screenLeft.innerHTML = ''
-                } else {
-                    msgStoreRight = [];
-                    screenRight.innerHTML = ''
-                }
-            }
+          actor.style.fontStyle = 'italic';
         }
+      } else {
+        if (side === 'left') {
+          msgStoreLeft = [];
+          screenLeft.innerHTML = '';
+        } else {
+          msgStoreRight = [];
+          screenRight.innerHTML = '';
+        }
+      }
     }
+  };
 }
 
 function sendMsgForLeft() {
-    if (inputLeft.value) {
-        msgStoreLeft.push(`<div class='msg msg__right'><span style='font-style:${inputLeft.style.fontStyle || ""}; font-weight:${inputLeft.style.fontWeight || ""}'>${inputLeft.value}</span></div>`);
-        msgStoreRight.push(`<div class='msg msg__left'><span style='font-style:${inputLeft.style.fontStyle || ""}; font-weight:${inputLeft.style.fontWeight || ""}'>${inputLeft.value}</span></div>`);
-        screenLeft.innerHTML = msgStoreLeft.join(' ');
-        screenRight.innerHTML = msgStoreRight.join(' ');
-        inputLeft.value = '';
-        inputLeft.focus();
-    }
+  if (inputLeft.value) {
+    msgStoreLeft.push(
+      `<div class='msg msg__right'><span style='font-style:${
+        inputLeft.style.fontStyle || ""
+      }; font-weight:${inputLeft.style.fontWeight || ""}'>${
+        inputLeft.value
+      }</span></div>`
+    );
+    msgStoreRight.push(
+      `<div class='msg msg__left'><span style='font-style:${
+        inputLeft.style.fontStyle || ""
+      }; font-weight:${inputLeft.style.fontWeight || ""}'>${
+        inputLeft.value
+      }</span></div>`
+    );
+    screenLeft.innerHTML = msgStoreLeft.join(' ');
+    screenRight.innerHTML = msgStoreRight.join(' ');
+    inputLeft.value = '';
+    inputLeft.focus();
+  }
 }
 
 function sendMsgForRight() {
-    if (inputRight.value) {
-        msgStoreLeft.push(`<div class='msg msg__left'><span style='font-style:${inputRight.style.fontStyle || ""}; font-weight:${inputRight.style.fontWeight || ""}'>${inputRight.value}</span></div>`);
-        msgStoreRight.push(`<div class='msg msg__right'><span style='font-style:${inputRight.style.fontStyle || ""}; font-weight:${inputRight.style.fontWeight || ""}'>${inputRight.value}</span></div>`);
-        screenLeft.innerHTML = msgStoreLeft.join(' ');
-        screenRight.innerHTML = msgStoreRight.join(' ');
-        inputRight.value = '';
-        inputRight.focus();
-    }
+  if (inputRight.value) {
+    msgStoreLeft.push(
+      `<div class='msg msg__left'><span style='font-style:${
+        inputRight.style.fontStyle || ""
+      }; font-weight:${inputRight.style.fontWeight || ""}'>${
+        inputRight.value
+      }</span></div>`
+    );
+    msgStoreRight.push(
+      `<div class='msg msg__right'><span style='font-style:${
+        inputRight.style.fontStyle || ""
+      }; font-weight:${inputRight.style.fontWeight || ""}'>${
+        inputRight.value
+      }</span></div>`
+    );
+    screenLeft.innerHTML = msgStoreLeft.join(' ');
+    screenRight.innerHTML = msgStoreRight.join(' ');
+    inputRight.value = '';
+    inputRight.focus();
+  }
 }
