b21ed002622376be5f9f7f9df3ac62b9f59a7d14
Mark <81944563+TungNguyen304@users.noreply.github.com>
2023-02-08 13:55:30 +0700

    Fix code(add end line, split function,add warning for each input, change type input, rename validation accordingly)

--- source/baitap4/main.js
+++ source/baitap4/main.js
@@ -11,8 +11,13 @@ const resetBtn = $('.action .reset');
 const infoListShow = $$('.info_item span');
 const avtShow = $('.screen_avt img');
 const descShow = $('.screen_avt p');
-const error = $('form>p');
-const textError = $('form>p span');
+const errorList = $$('form>p');
+const fullname = $('.form_item input#fullname');
+const email = $('.form_item input#email');
+const phone = $('.form_item input#phone');
+const birthday = $('.form_item input#birthday');
+const password = $('.form_item input#password');
+const confirmPassword = $('.form_item input#confirm');
 
 let url = '';
 const verifyEmail = /^\S+@([a-zA-Z]+\.)+[a-zA-z]{2,4}$/;
@@ -20,7 +25,6 @@ const verifyPhone = /^0[0-9]{9}$/;
 const verifyBirthday = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
 const verifyPass =
   /^[a-zA-Z](?=.*[a-z])(?=.[A-Z]*)(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,29}$/;
-const verifyName = /^[a-zA-z\s]{3,150}$/;
 
 uploadButton.onclick = () => {
   upload.click();
@@ -52,108 +56,72 @@ window.onkeydown = (e) => {
 
 function addProfile(e) {
   e.preventDefault();
-  let warning = 'Xin hãy nhập đầy đủ thông tin!';
+  let warning = {
+    desc: 'Xin hãy nhập đầy đủ thông tin!',
+    type: 'name',
+  };
   const Person = {
-    fullname: infoList[0].value,
-    email: infoList[1].value,
-    phone: infoList[2].value,
-    birthday: infoList[3].value,
-    password: infoList[4].value,
-    confirm: infoList[5].value,
+    fullname: fullname.value,
+    email: email.value,
+    phone: phone.value,
+    birthday: birthday.value,
+    password: password.value,
+    confirm: confirmPassword.value,
   };
 
-  const validate = Object.keys(Person).every((item) => {
+  const validateEmpty = Object.keys(Person).filter((item) => {
     if (!Person[item]) {
+      showError(item, `${item} không được để trống.`);
+      return true;
+    }
+  });
+
+  const validateValid = Object.keys(Person).filter((item) => {
+    if (Person[item] && item === 'fullname') {
+      return validateName(Person[item], item);
+    }
+    if (Person[item] && item === 'email') {
+      return validateEmail(Person[item], item);
+    }
+    if (Person[item] && item === 'phone') {
+      return validatePhone(Person[item], item);
+    }
+    if (Person[item] && item === 'birthday') {
+      return validateBirthday(Person[item], item);
+    }
+    if (Person[item] && item === 'password') {
+      return validatePassword(Person[item], item);
+    }
+    if (Person[item] && item === 'confirm') {
+      return validateComfirm(Person[item], Person['password'], item);
+    }
+  });
+
+  const validateAvt = (function () {
+    if (!upload.files[0]) {
+      showError('img', 'Chọn avt trước khi add.');
+      uploadButton.style.border = '1px solid red';
       return false;
     }
+    hideError('img');
+    uploadButton.style.border = '1px solid black';
     return true;
-  });
+  })();
 
-  infoList.forEach((item) => {
-    if (!validate && !item.value) {
-      item.style.border = '1px solid red';
-    } else {
-      item.style.border = '1px solid black';
-    }
-  });
-
-  const validate1 =
-    validate &&
-    Object.keys(Person).every((item) => {
-      if (item === 'fullname') {
-        if (!verifyName.test(Person[item])) {
-          warning = 'Full name không hợp lệ.';
-          return false;
-        }
-      } else {
-        if (item === 'email') {
-          if (!verifyEmail.test(Person[item])) {
-            warning = 'Email không hợp lệ.';
-            return false;
-          }
-        } else {
-          if (item === 'phone') {
-            if (!verifyPhone.test(Person[item])) {
-              warning = 'Phone không hợp lệ.';
-              return false;
-            }
-          } else {
-            if (item === 'birthday') {
-              if (!verifyBirthday.test(Person[item])) {
-                warning = 'Birthday phải có dạng dd/mm/YYYY.';
-                return false;
-              }
-            } else {
-              if (item === 'password') {
-                if (!verifyPass.test(Person[item])) {
-                  warning =
-                    'Password: 8-30 kí tự, bắt đầu bằng chữ cái, có chứa kí tự đặc biệt, số, chữ viết hoa.';
-                  return false;
-                }
-              } else {
-                if (Person[item] !== Person['password']) {
-                  warning = 'Confirm password không khớp.';
-                  return false;
-                }
-              }
-            }
-          }
-        }
-      }
-      return true;
-    });
-
-  let validate2 = false;
-  if (validate1) {
-    if (!upload.files[0]) {
-      warning = 'Chọn avt trước khi add.';
-      uploadButton.style.border = '1px solid red';
-      validate2 = false;
-    } else {
-      validate2 = true;
-    }
-  }
-  if (validate2) {
+  if (validateEmpty.length === 0 && validateValid.length === 0 && validateAvt) {
     Object.keys(Person).forEach((item, index) => {
       if (infoListShow[index]) {
         if (item === 'fullname') {
-          let listName = Person[item].split(' ');
-          listName = listName.map((i, index) => {
-            const name = i.toLowerCase();
-            return name.replace(name[0], name.charAt(0).toUpperCase());
-          });
-          infoListShow[index].textContent = listName.join(' ');
-        } else {
-          if (item === 'phone') {
-            infoListShow[index].textContent =
-              Person[item].slice(0, 3) +
-              '-' +
-              Person[item].slice(3, 6) +
-              '-' +
-              Person[item].slice(6);
-          } else {
-            infoListShow[index].textContent = Person[item];
-          }
+          renderFullName(Person[item], index);
+        }
+        if (item === 'phone') {
+          renderPhone(Person[item], index);
+        }
+        if (item === 'birthday') {
+          infoListShow[index].textContent = Person[item].split('-').reverse().join('/');
+        }
+        if (item !== 'fullname' && item !== 'phone' && item !== 'birthday') {
+          infoListShow[index].textContent = Person[item];
         }
       }
       infoList[index].value = '';
@@ -164,10 +132,9 @@ function addProfile(e) {
       avtShow.src = url;
     }
     upload.value = null;
-    error.style.display = 'none';
-  } else {
-    textError.textContent = warning;
-    error.style.display = 'block';
+    errorList.forEach((item) => {
+      item.style.display = 'none';
+    });
   }
 }
 
@@ -183,9 +150,103 @@ function resetProfile() {
   avtShow.src = '';
   descShow.style.display = 'unset';
   upload.value = null;
-  error.style.display = 'none';
   infoList.forEach((item) => {
     item.style.border = '1px solid black';
   });
+  errorList.forEach((item) => {
+    item.style.display = 'none';
+  });
   uploadButton.style.border = '1px solid black';
+}
+
+function showError(type, desc) {
+  $(
+    `form .warning_${type}`
+  ).innerHTML = `<i class='fa-solid fa-triangle-exclamation'></i><span>${desc}</span>`;
+  $(`form .warning_${type}`).style.display = 'block';
+  $(`.form_item input#${type}`).style.border = '1px solid red';
+}
+
+function hideError(type) {
+  $(`form .warning_${type}`).style.display = 'none';
+  $(`.form_item input#${type}`).style.border = '1px solid black';
+}
+
+function renderFullName(name, index) {
+  let listName = name.split(' ');
+  listName = listName.map((i) => {
+    const name = i.toLowerCase();
+    return name.replace(name[0], name.charAt(0).toUpperCase());
+  });
+  infoListShow[index].textContent = listName.join(' ');
+}
+
+function renderPhone(phone, index) {
+  infoListShow[index].textContent =
+    phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
+}
+
+function validateName(name, item) {
+  if (name.length > 150) {
+    showError(item, 'Full name không hợp lệ.');
+    return true;
+  }
+  hideError(item);
+  return false;
+}
+
+function validateEmail(email, item) {
+  if (!verifyEmail.test(email)) {
+    showError(item, 'Email không hợp lệ.');
+    return true;
+  }
+  hideError(item);
+  return false;
+}
+
+function validatePhone(phone, item) {
+  if (!verifyPhone.test(phone)) {
+    showError(item, 'Phone number không hợp lệ.');
+    return true;
+  }
+  hideError(item);
+  return false;
+}
+
+function validateBirthday(birthday, item) {
+  if (!verifyBirthday.test(birthday.split('-').reverse().join('/'))) {
+    showError(item, 'Birthday phải có dạng dd/mm/YYYY.');
+    return true;
+  }
+  hideError(item);
+  return false;
+}
+
+function validatePassword(password, item) {
+  if (!verifyPass.test(password)) {
+    showError(
+      item,
+      'Password: 8-30 kí tự, bắt đầu bằng chữ cái, có chứa kí tự đặc biệt, số, chữ viết hoa.'
+    );
+    return true;
+  }
+  hideError(item);
+  return false;
+}
+
+function validateComfirm(password, oldPass, item) {
+  if (password !== oldPass) {
+    showError(item, 'Confirm password không khớp.');
+    return true;
+  } else {
+    if (!verifyPass.test(password)) {
+      showError(
+        item,
+        'Password: 8-30 kí tự, bắt đầu bằng chữ cái, có chứa kí tự đặc biệt, số, chữ viết hoa.'
+      );
+      return true;
+    }
+    hideError(item);
+    return false;
+  }
 }
